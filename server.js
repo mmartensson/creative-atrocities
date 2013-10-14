862e3fc60d66e99701b2a0b97d433ec50377600f
Markus Mårtensson <markus@mayam.com>
2013-10-15 00:12:43 +0200

    Sorting decks based on name.

--- server.js
+++ server.js
@@ -44,6 +44,13 @@ function terseDecks() {
             stripped.push({ id: id, name: deck.name });
         }
     }
+
+    stripped.sort(function(a,b) {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+    });
+
     return stripped;
 }
 
